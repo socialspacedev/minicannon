dfefb308e2fef91ab164311557778b7970716e0a
Andrew Long <andrew@cloudcannon.com>
2024-01-10 03:12:30 +0000

    Updated 1 file via CloudCannon.

--- .eleventy.js
+++ .eleventy.js
@@ -78,7 +78,7 @@ module.exports = function (eleventyConfig) {
     let metadata = await Image(inputFilePath, {
       widths: [400, 800, 1600],
       formats: ["avif", "webp", "svg", "jpeg"],
-      outputDir: "./optimized/",
+      outputDir: "./public/",
       urlPath: "/optimized/",
       svgShortCiruit: "size",
       // svgCompressionSize: "br",
