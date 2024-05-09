d57b6b485d6adbe92cf33de9d3bf8d568d21355f
Andrew Long <andrew@cloudcannon.com>
2024-05-09 04:18:39 +0000

    Updated 1 file via CloudCannon.

--- .eleventy.js
+++ .eleventy.js
@@ -84,17 +84,7 @@ module.exports = function (eleventyConfig) {
   });
 		
   // Exclude drafts from production
-	eleventyConfig.addGlobalData("eleventyComputed.permalink", function () {
-		return (data) => {
-			if (!data.published && process.env.production) {
-				return false;
-				console.log(data.article.title);
-			}
-
-			  return `/${data.article}/`;
-		  };
-	  }
-	);
+  
 
 	eleventyConfig.addGlobalData(
 		"eleventyComputed.eleventyExcludeFromCollections",
@@ -105,6 +95,7 @@ module.exports = function (eleventyConfig) {
 				}
 
 			  return data.eleventyExcludeFromCollections;
+			    	console.log(data.article.title);
 			};
 		}
 	);
